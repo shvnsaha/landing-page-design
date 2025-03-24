5989f15d3192ca7342bbae915ddc0f6dadaed68c
shvnsaha <shovan.css2018@gmail.com>
2025-03-24 12:21:44 +0600

    sub heading done

--- src/components/DesignSection.jsx
+++ src/components/DesignSection.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 
 
 const DesignSection = () => {
@@ -12,14 +13,25 @@ const DesignSection = () => {
 
                 {/* content grid */}
                 <div className="grid grid-cols-1 lg:grid-cols-2 gap-16">
-                 {/* content */}
-                 <div className="flex flex-col justify-between gap-16">
-                    {/* sub heading */}
-                    <div>Sub heading</div>
+                    {/* content */}
+                    <div className="flex flex-col justify-between gap-16">
+                        {/* sub heading */}
+                        <div>
+                            <p className="text-2xl text-gray-300">
+                                Rethink the web dev cycle with CodeTutor. Give your design and marketing teams the power to launch sophisticated sites quickly — so your dev team can focus on more complex work, not pixel-perfect revisions.
+                            </p>
+                            <Link
+                                to="/dashboard/signup"
+                                className="inline-flex items-center px-8 py-4 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition-colors mt-8"
+                            >
+                                Get started
+                                <span className="font-normal ml-1">— it's free</span>
+                            </Link>
+                        </div>
 
-                    {/* accrodian */}
-                    <div>accrodian</div>
-                 </div>
+                        {/* accrodian */}
+                        <div>accrodian</div>
+                    </div>
 
 
                     {/* video */}
