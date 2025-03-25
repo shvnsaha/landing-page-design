6a46a9594b5f6be5284fa37b104f82e8b43cc8c9
shvnsaha <shovan.css2018@gmail.com>
2025-03-25 12:49:13 +0600

    update code

--- src/components/DesignSection.jsx
+++ src/components/DesignSection.jsx
@@ -1,7 +1,75 @@
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router";
 
 
 const DesignSection = () => {
+
+    const tabsData = [
+        {
+          id: 'tab1',
+          title: 'Design without limits',
+          subtitle: 'CodeTutor puts the power of code into a visual canvas so every team can create stunning websites quickly — and extend the power of their work with custom code.',
+          video: 'https://dhygzobemt712.cloudfront.net/Web/home/2024-wxp/features/design.mp4',
+          poster: 'https://cdn.prod.website-files.com/66e88746834b80507cdf7933/670570322cf4b274d716fed4_design-without-limits.avif',
+          cta: {
+            text: 'Discover Designer',
+            link: '/designer'
+          }
+        },
+        {
+          id: 'tab2',
+          title: 'Create complex, rich interactions',
+          subtitle: 'Design scroll-based and multi-step interactions and animations and easily work with GSAP, Spline, 3D, Lottie, and Rive — all without even thinking about code.',
+          video: 'https://dhygzobemt712.cloudfront.net/Web/home/2024-wxp/features/complex-rich-animations.mp4',
+          poster: 'https://cdn.prod.website-files.com/66e88746834b80507cdf7933/67057032ad30932a68cd9d18_animations.avif',
+          cta: {
+            text: 'Discover Interactions',
+            link: '/interactions-animations'
+          }
+        },
+        {
+          id: 'tab3',
+          title: 'Empower everyone to build on-brand sites',
+          subtitle: 'Give less technical teams the tools they need to build confidently with reusable design systems powered by variables, components, and libraries.',
+          video: 'https://dhygzobemt712.cloudfront.net/Web/home/2024-wxp/features/build-on-brand-sites.mp4',
+          poster: 'https://cdn.prod.website-files.com/66e88746834b80507cdf7933/67058d52036e5522e27966de_build-on-brand.avif',
+          cta: {
+            text: 'Discover page building',
+            link: '/page-building'
+          }
+        },
+        {
+          id: 'tab4',
+          title: 'Create even faster with the CodeTutor AI Assistant',
+          subtitle: "CodeTutor's AI Assistant lets you build faster and more efficiently by applying your site's existing design system to new page sections.",
+          video: 'https://dhygzobemt712.cloudfront.net/Web/home/2024-wxp/features/webflow-ai-assistant.mp4',
+          poster: 'https://cdn.prod.website-files.com/66e88746834b80507cdf7933/670570323f08ce0aed3368e4_ai-assistant.avif',
+          cta: {
+            text: 'Discover AI Assistant',
+            link: '/ai'
+          }
+        }
+      ];
+
+
+      const [activeTab, setActiveTab] = useState('tab1');
+      const [isPlaying, setIsPlaying] = useState(true);
+      const [progress, setProgress] = useState(0);
+      const progressInterval = useRef(null);
+      const PROGRESS_DURATION = 10000; // 10 seconds for each tab
+      const UPDATE_INTERVAL = 100; // Update progress every 100ms
+
+      useEffect(()=>{
+          startProgressTimer();
+          return () => clearInterval(progressInterval.current)
+      },[activeTab])
+
+      const startProgressTimer = () =>{
+          setProgress(0);
+          clearInterval(progressInterval.current)
+          progressInterval.current = setInterval
+      }
+
     return (
         <div className="overflow-hidden py-24 bg-black text-white">
             <div className="primary-container">
